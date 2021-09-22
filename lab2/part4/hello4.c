d7e7a8ec2aba40c27ac0dc55c2fd5219916d7f98
Adam Bujak <adamjbujak@gmail.com>
2021-09-21 20:16:35 -0400

    fixed

--- lab2/part4/hello4.c
+++ lab2/part4/hello4.c
@@ -5,7 +5,6 @@
 #define DRIVER_AUTHOR "Adam+Harneet"
 #define DRIVER_DESC "4AA4 Lab2 Part3"
 
-static int foo __initdata = 2;
 static char *my_name = "harneet";
 
 static int __init init_custom_module(void)
@@ -22,11 +21,9 @@ static void __exit cleanup_custom_module(void)
 module_init(init_custom_module);
 module_exit(cleanup_custom_module);
 
-module_param(my_name, charp, 0000)
-MODULE_PARM_DESC(my_name, "Your name string")
+module_param(my_name, charp, 0000);
+MODULE_PARM_DESC(my_name, "Your name string");
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
-
-
